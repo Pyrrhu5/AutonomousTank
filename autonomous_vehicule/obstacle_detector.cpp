e8443198dd92ab0b65ba6b8a08ad79fdf9858751
Aymeric Deschard <aymeric.deschard@disroot.org>
2021-12-13 18:19:53 +0100

    Implement closest available direction

--- autonomous_vehicule/obstacle_detector.cpp
+++ autonomous_vehicule/obstacle_detector.cpp
@@ -1,4 +1,3 @@
-/* #include <Vector.h> */
 #include <Arduino.h>
 #include <Servo.h>
 
@@ -7,9 +6,11 @@ class ObstacleDetector {
 		int echoPin;
 		int triggerPin;
 		Servo servo;
+		float minDistance;
+		float maxDistance;
 
 	public:
-		void begin(int servoPin, int echoPin, int triggerPin){
+		void begin(int servoPin, int echoPin, int triggerPin, float minDistance, float maxDistance){
 			// Servo Motor
 			pinMode(servoPin, OUTPUT);
 			this->servo.attach(servoPin);
@@ -22,13 +23,17 @@ class ObstacleDetector {
 			this->triggerPin = triggerPin;
 			pinMode(this->triggerPin, OUTPUT);
 			digitalWrite(this->triggerPin, LOW);
+			this->minDistance = minDistance;
+			this->maxDistance = maxDistance;
 		}
 
+	private:
+		/* Set the angle of the servo motor from 0 deg (left) to 180 (right) */
 		void set_angle(int degree){
 			this->servo.write(degree);
 		}
 
-
+	public:
 		/* Get the distance of the nearest object in meters, max is 0.75m */
 		float get_distance(){
 			digitalWrite(this->triggerPin, LOW);
@@ -37,17 +42,21 @@ class ObstacleDetector {
 			delayMicroseconds(15);
 			digitalWrite(this->triggerPin, LOW);
 
-			return (pulseIn(this->echoPin, HIGH)) * 0.0001657;
+			return (pulseIn(this->echoPin, HIGH)) * 0.0001657; // nice number to convert to m
 		}
 
 
 		/* Performs a 180deg scan by precision<unsigned int> degrees increments
-			And return a pointer to the array of results from 0deg to 180deg 
+			and return a pointer to the array of results from 0deg to 180deg.
+			In meters.
 		*/
 		float *scan(unsigned int precision=45){
 			int nIter = (180/precision) + 1;
 			int currentAngle = 0;
 			float *scanData = new float[nIter];
+			// The first rotation takes more time
+			this->set_angle(currentAngle);
+			delay(100);
 
 			for (int i = 0; i < nIter; ++i){
 				this->set_angle(currentAngle);
@@ -87,6 +96,39 @@ class ObstacleDetector {
 			}
 
 			return bestAngle;
+		}
+
+		/* Performs a 180deg scan by precision<unsigned int> degrees increments
+			and output the angle which is the closest to 90 deg and is above 
+			the minimum distance.
+			Returns -1 if no acceptable angle has been found;
+ 		*/
+		int acceptable_angle(unsigned int precision = 45){
+			float *scanData = this->scan(precision);
+			int arraySize = (180 / precision) + 1;
+			int middleIndex = (arraySize - 1) / 2;
+
+			// If there is room to move straight, let's go straight
+			if (scanData[middleIndex] > this->minDistance) return 90;
+
+			int bestIndex = -1;
+			// Minimum acceptable index from 0 to middle
+			for (int i = (middleIndex - 1); i >= 0; i--){
+				if (scanData[i] > this->minDistance) {
+					bestIndex = i;
+				}
+			}
+			// Minimum acceptable index from middle to arraySize
+			for (int i = (middleIndex + 1); i < arraySize; i++){
+				// Take the index only if higher than the previous bestIndex
+				if (scanData[i] > this->minDistance && (arraySize - 1 - i) > bestIndex){
+					return i * precision;
+				}
+			}
+			// Minimum acceptable angle is from 0 to middle
+			// or none
+			if (bestIndex != -1) return bestIndex * precision;
+			else return -1;
 		}
 
 };
